4fb576f757e74d7c527ea650caef0e269851293b
Andrii Tokar <andrii.tokar@gmail.com>
2023-11-23 14:47:25 +0200

    [2023-11-23] add func slice.Value()

---
+++ slice/slice.go
@@ -0,0 +1,16 @@
+package slice
+
+import "reflect"
+
+func Value[V any](sl *[]V, k int, def V) (v V) {
+	sv := *sl
+	rv := reflect.ValueOf(sv)
+
+	if rv.Len() <= k {
+		v = def
+	} else {
+		v = sv[k]
+	}
+
+	return
+}
